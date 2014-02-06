017b3295aae5d745603b504312f96258d0d37396
Bruno Thomas <bruno@barreverte.fr>
2014-02-06 23:16:42 +0100

    adding title/url in feed list

--- myrefs/site/static/js/app.js
+++ myrefs/site/static/js/app.js
@@ -41,7 +41,7 @@ var app = app || {};
             this.model.fetch({reset: true});
         },
         appendRssFeed: function (rssFeed) {
-            $('ul', this.el).append("<li>" + rssFeed.get('url') + "</li>");
+            $('ul', this.el).append("<li><a href='" + rssFeed.get('main_url') + "'>" + rssFeed.get('title') + "</a></li>");
         },
         render: function () {
             $(this.el).find('#rssfeeds').append("<ul></ul>");
