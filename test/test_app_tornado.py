687b41c677b62ea55e03006f67c5fb07634971b4
Bruno Thomas <bruno@barreverte.fr>
2014-03-22 14:51:34 +0100

    tornado : checking that the feed is in the SSE events

--- test/test_app_tornado.py
+++ test/test_app_tornado.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from Queue import Queue
 from app_tornado import CheckRssFeedsHandlder
 import tornado
@@ -15,18 +16,22 @@ class MockFeedsRepository(object):
 
 
 class MockRequestHandler(tornado.web.RequestHandler):
-    def initialize(self, requests_queue):
+    def initialize(self, requests_queue, response_queue):
         self.requests = requests_queue
+        self.responses = response_queue
 
     def get(self, path):
         self.requests.put(path)
+        self.write(self.responses.get(0))
 
 
 class TestCheckRssFeedsHandlder(AsyncHTTPTestCase):
     def setUp(self):
         super(TestCheckRssFeedsHandlder, self).setUp()
         self.requests_queue = Queue()
-        self.feed_provider = HTTPServer(Application([(r'/(.*)', MockRequestHandler, {'requests_queue': self.requests_queue})]), io_loop=self.io_loop)
+        self.response_queue = Queue()
+        self.feed_provider = HTTPServer(Application([(r'/(.*)', MockRequestHandler,
+                          {'requests_queue': self.requests_queue, 'response_queue': self.response_queue})]), io_loop=self.io_loop)
         self.feed_provider.listen(12345)
 
     def get_app(self):
@@ -35,7 +40,25 @@ class TestCheckRssFeedsHandlder(AsyncHTTPTestCase):
                 [{'id': 'md5', 'url': 'http://localhost:12345/feeds', 'main_url': 'main_url'}])})])
 
     def test_feeds_checked_with_get(self):
-        self.http_client.fetch(self.get_url('/updatefeeds'), self.stop)
+        self.response_queue.put(FEED_HEADER + '<item><link>my_article_url</link></item>' + FEED_FOOTER)
+        traceback_future = self.http_client.fetch(self.get_url('/updatefeeds'), self.stop)
         self.wait()
         self.assertEquals(1, self.requests_queue.qsize())
         self.assertEquals('feeds', self.requests_queue.get())
+        self.assertIn("my_article_url", traceback_future.result().body)
+
+FEED_HEADER = """
+<?xml version="1.0" encoding="UTF-8"?>
+<rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:wfw="http://wellformedweb.org/CommentAPI/"
+     xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:atom="http://www.w3.org/2005/Atom"
+     xmlns:sy="http://purl.org/rss/1.0/modules/syndication/" xmlns:slash="http://purl.org/rss/1.0/modules/slash/">
+    <channel><title>Feed Title</title>
+        <atom:link href="http://feed_url" rel="self" type="application/rss+xml"/>
+        <link>http://blog_url</link>
+        <description>feed description</description>
+"""
+
+FEED_FOOTER = """
+</channel>
+</rss>
+"""
